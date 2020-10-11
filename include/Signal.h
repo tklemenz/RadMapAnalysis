25c6c646b70cab573c57c8ea3868bfd0dee745ab
Thomas Klemenz <thomas.klemenz@tum.de>
2020-10-11 13:22:02 +0200

    readability of Signal.h

--- include/Signal.h
+++ include/Signal.h
@@ -11,9 +11,9 @@ class Signal
   Signal(const Double_t tot, const Double_t timeStamp, const Int_t signalNr);
   virtual ~Signal();
 
-  inline void setToT(Double_t tot) { mTot = tot; }
-  inline void setTimeStamp(Double_t timeStamp) { mTimeStamp = timeStamp; }
-  inline void setSignalNr(Int_t signalNr) { mSignalNr = signalNr; }
+  inline void setToT				(Double_t tot) 				{ mTot = tot; }
+  inline void setTimeStamp	(Double_t timeStamp) 	{ mTimeStamp = timeStamp; }
+  inline void setSignalNr		(Int_t signalNr) 			{ mSignalNr = signalNr; }
 
   inline Double_t getToT()       const { return mTot; }
   inline Double_t getTimeStamp() const { return mTimeStamp; }
@@ -21,9 +21,9 @@ class Signal
 
  private:
 
-	Double_t mTot;
-	Double_t mTimeStamp;
-	Int_t mSignalNr;
+	Double_t 	mTot;
+	Double_t 	mTimeStamp;
+	Int_t 		mSignalNr;
 
 	ClassDef(Signal,1);
 };
