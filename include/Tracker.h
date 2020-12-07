f5d725c926b344549b9510900d4f32826019ccd6
Thomas Klemenz <thomas.klemenz@tum.de>
2020-12-07 18:27:02 +0100

    Add run functions to tracker. They still do nothing.

--- include/Tracker.h
+++ include/Tracker.h
@@ -6,6 +6,10 @@
 
 /// Tracker class to assign clusters from CTSEventClusters to tracks.
 /// Ideally a track consists of 4 clusters, one in each layer.
+///
+/// Option 1: The Tracker takes a CTSEventClusters and find tracks from the clusters.
+/// Option 2: The Tracker gets a vector of Clusters and does the tracking.
+///           Then the user needs to make sure that the clusters are all valid (e.g. not 5 seconds apart).
 
 class Tracker
 {
@@ -13,8 +17,13 @@ class Tracker
   Tracker() = default;
   ~Tracker() = default;
 
+  /// run the tracking
+  void run(const CTSEventClusters& event);
+  void run(const std::vector<Cluster>& clusters);
+
  private:
 
+  std::vector<Track> mTracks{}; ///< contains all found tracks
 
   ClassDef(Tracker,1);
 };
--- src/Tracker.cxx
+++ src/Tracker.cxx
@@ -1,3 +1,15 @@
 #include "Tracker.h"
 
 ClassImp(Tracker);
+
+//________________________________________________________________________________
+void Tracker::run(const std::vector<Cluster>& clusters)
+{
+  /// do something
+}
+
+//________________________________________________________________________________
+void Tracker::run(const CTSEventClusters& event)
+{
+  Tracker::run(event.getClusters());
+}
